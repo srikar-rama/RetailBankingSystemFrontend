e36b7344c35becdf4d22e2171f0685ea1acb1245
srikar-rama <srikarrama555@gmail.com>
2022-07-04 23:24:52 +0530

    updated

---
+++ src/app/models/account/account.module.ts
@@ -0,0 +1,22 @@
+export interface AccountDto {
+  long accountId;
+	
+	String customerId;
+
+	double currentBalance;
+	
+  String accountType;
+	
+  String ownerName;
+}
+export class AccountDto {
+ public long accountId;
+	
+ public String customerId;
+
+ public	double currentBalance;
+	
+ public String accountType;
+	
+ public String ownerName;
+}
